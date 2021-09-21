d2a402a2c705aa03c25671132d4f219e94da6452
Endson <endsonsantosconta@gmail.com>
2021-09-21 11:34:53 -0300

    Modificações: 084.py

--- CursoEmVídeo/Mundo3/Aula 18 - Listas (Parte 2)/Exe084.py
+++ CursoEmVídeo/Mundo3/Aula 18 - Listas (Parte 2)/Exe084.py
@@ -1,11 +1,17 @@
+#Exercício Python 084: Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. 
+# No final, mostre:                                                                                                    
+# A) Quantas pessoas foram cadastradas.                                                                                                                
+# B) Uma listagem com as pessoas mais pesadas.                                                                                                    
+# C) Uma listagem com as pessoas mais leves.
+
 pessoas = list()
 dados = list()
 maior = menor = 0
 
-while True:
+while True: # Criando um laço infinito para armazenar dados
     dados.append(str(input('Nome: ')))
     dados.append(float(input('Peso: ')))
-    if len(pessoas) == 0:
+    if len(pessoas) == 0: # Criando uma Codição para o maior e menor peso
         maior = menor = dados[1]
     else:
         if dados[1] > maior:
@@ -14,9 +20,11 @@ while True:
             menor = dados[1]
     pessoas.append(dados)
     dados.clear()
-    resposta = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
-    if resposta in 'Nn':
+    resposta = str(input('Quer continuar? [S/N]: ')).upper().strip()[0] # Codição para quebra ou não do laço
+    if resposta in 'Nn': # qubrando o laço infinito
         break
+
+# Imprimindo resultado da questão
 
 print()
 print('-='*30)
